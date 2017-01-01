1499557561df70ad1c25e3ea995dffe913d66915
TURuibo <ruibo.tu@gmail.com>
2017-01-01 15:45:36 +0100

    Separate the file

---
+++ src/RandomForest.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numpy.random as rnd
+from scipy import stats
+from Tree import *
+from DataPool import *
+
+class random_forest:  # {
+    def __init__(number_of_trees, F, min_leaf_size=1, f_num=VR, f_cat=IG):  # {
+        pass
+
+    # def add_data(  # n x m feature data, m variable/feature type, n output, training_split = 0.1)
+    # #     pass
---
+++ src/node.py
@@ -0,0 +1,43 @@
+import numpy as np
+import numpy.random as rnd
+from scipy import stats
+import os
+from DataPool import *
+
+class node:  #{
+    # Node class
+    # left and right point to children
+    # is_num decides if the split is on a numerical variable
+    # split feature = index of feature that this node splits on
+    # split_value = the split threshold for feature
+    # value = if this is a leaf, the value that this node returns
+    def __init__(self, is_num):
+        self.left = None;
+        self.right = None;
+        self.is_num = is_num;
+        self.split_feature = None;
+        self.split_value = None;
+        self.value = None;
+
+    def predict(self, data_point):  # {
+        if (self.is_leaf()):  # {
+            return self.value;
+        else:
+            if (self.compare(data_point)):
+                return self.right.predict(data_point);
+            else:
+                return self.left.predict(data_point);
+                # }
+
+    # }
+
+    def is_leaf(self):
+        return (self.left == None and self.right == None)
+
+    def compare(self, data_point, is_num):
+        # helper function to do a split, based on is_num
+        if (is_num):
+            return data_point[self.split_feature] > self.split_value;
+        else:
+            return data_point[self.split_feature] == self.split_value;
+#}
